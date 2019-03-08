4bb1a5f30c302d5f7f7937dc679c44ab44f52ffe
Zhyhariev Mikhail <zhygarevmikhail@gmail.com>
2019-03-08 22:46:36 +0200

    Fix stylesheet hot relaod bug

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 const config = require('./webpack.base.config.js');
 
 config.mode = 'development';
+
+config.watch = true;
 
 config.entry = [
   'webpack-dev-server/client?http://localhost:3000',
@@ -18,21 +19,21 @@ config.output.publicPath = 'http://localhost:3000/static/';
 
 config.plugins = config.plugins.concat([
   new webpack.HotModuleReplacementPlugin(),
+
+  new webpack.NamedModulesPlugin(),
+
   new webpack.NoEmitOnErrorsPlugin(),
+
   new BundleTracker({filename: './webpack-stats.json'}),
-  new ExtractTextPlugin('./app/css/style.css'),
 ]);
 
 config.module.rules.push({
   test: /\.(sa|sc|c)ss$/,
-  use: ExtractTextPlugin.extract({
-    publicPath: 'http://127.0.0.1:8000/',
-    fallback: 'style-loader',
-    use: [
-      'css-loader',
-      'sass-loader',
-    ]
-  })
+  use: [
+    'style-loader',
+    'css-loader',
+    'sass-loader',
+  ]
 });
 
 module.exports = config;
